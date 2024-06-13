c15eb0e0a55dcf7e041df520f0e0271ff12885da
2405-011-pw <walypa56@gmail.com>
2024-06-13 14:50:09 +0100

    Made JokeArray

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import React from "react"
 import Joke from "./components/Joke.jsx"
+import JokesData from "./components/JokesData.jsx"
 
 export default function App() {
+    const JokeElements = JokesData.map(joke => <Joke setup={joke.setup} 
+        punchline={joke.punchline}/>)
     return(
         <>
             <h1>My Skeleton Joke Page</h1>
-            <Joke 
-                setup="How many skeleton jokes are on this page?"
-                punchline="A Skele-tonne"
-            />
-            <Joke 
-                setup="What do you call a skeleton who doesn't style his page with css?"
-                punchline="Lazy bones"
-            />
-            <Joke 
-                setup="Why did the skeleton not do pair-programming?"
-                punchline="He has no body to code with"
-            />
+            {JokeElements}
         </>
     )
 }
---
+++ src/components/JokesData.jsx
@@ -0,0 +1,22 @@
+export default [
+    {
+        setup: "How many skeleton jokes are on this page?",
+        punchline: "A Skele-tonne"
+    },
+    {
+        setup: "What do you call a skeleton who doesn't style his page with css?",
+        punchline: "Lazy bones"
+    },
+    {
+        setup: "Why did the skeleton not do pair-programming?",
+        punchline: "He has no body to code with"
+    },
+    {
+        setup: "Why was the skeleton afraid of object-orientated-programming?",
+        punchline: "He didn't have enough backbone"
+    },
+    {
+        setup: "How did the skeleton hack all the banks?",
+        punchline: "He had a skeleton SSH key"
+    }
+]
